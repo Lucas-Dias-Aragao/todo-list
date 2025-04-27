6e95847e56fa426b6618cbef390fe7878cc0df12
LucasDias <lucasd.aragao@hotmail.com>
2025-04-27 15:44:12 -0300

    Initial commit

---
+++ src/main/java/com/estudos/todolistservice/TodolistserviceApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.todolistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/todolistservice/TodolistserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.todolistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
